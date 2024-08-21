666dd43f211d3974195762b8e157db9c4f82d095
Ana Carolina <carol00_10@hotmail.com>
2024-08-21 18:15:40 -0300

    Organize files into folders
    for better code organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/body/home/home.component';
+import { NavigationComponent } from './components/body/navigation/navigation.component';
+import { NewsComponent } from './components/newsPage/news/news.component';
 import { HighlightsComponent } from './pages/highlights/highlights.component';
-import { HighlightItemComponent } from './components/highlight-item/highlight-item.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HighlightItemComponent } from './components/newsPage/highlight-item/highlight-item.component';
+import { FooterComponent } from './components/body/footer/footer.component';
+import { IndicadoresComponent } from './components/indicators/indicadores/indicadores.component';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,11 +21,14 @@ import { FooterComponent } from './components/footer/footer.component';
     HighlightsComponent,
     HighlightItemComponent,
     FooterComponent,
+    IndicadoresComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
