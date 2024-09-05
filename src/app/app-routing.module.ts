513541d23ac67c0b078cb89aa5669b59c60f3784
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-09-05 10:03:46 -0300

    19 page not found (#20)
    * Update navigation.component.html
    
    * create Page Not Found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomeComponent } from './components/body/home/home.component';
 import { HighlightsComponent } from './pages/highlights/highlights.component';
 import { AgendaComponent } from './pages/agenda/agenda.component';
 import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component:HomeComponent },
+  { path: '', component:HomeComponent, pathMatch: 'full', },
   { path: 'destaque', component: HighlightsComponent },
   { path: 'agenda-2030', component: AgendaComponent },
-  { path: 'sobre-a-ods', component: AboutComponent }
+  { path: 'sobre-a-ods', component: AboutComponent },
+  { path: '**', component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
